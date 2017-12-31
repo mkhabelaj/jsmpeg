1caebda1555e07d57d06b10e5cc3b5f9bc89d420
jackson <jackson@swordfish.co.za>
2017-12-31 17:19:18 +0200

    change variable name

--- startWebSocket.js
+++ startWebSocket.js
@@ -5,5 +5,5 @@ var WebSocketRelay = require('./WebSocketClass'),
     sh = require('sh');
 var element = process.argv[2];
 element = JSON.parse(element);
-test = new WebSocketRelay.WebSocketRelay(element.stream_secret,element.stream_port, element.websocket_port)
-test.initiateService();
+wsr = new WebSocketRelay.WebSocketRelay(element.stream_secret,element.stream_port, element.websocket_port)
+wsr.initiateService();
