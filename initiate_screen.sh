b43f8b9cc52d423b5f87f233c60f44383b116fd2
jackson <jackson@swordfish.co.za>
2017-12-31 16:29:09 +0200

    add extra logging infomation

--- initiate_screen.sh
+++ initiate_screen.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-
+##################################Initiate Screen####################################
+#name of the screen
 PROJECT_NAME=$1
+#name of the screen inside the project screen
 DESCRIPTOR=$2
+#Json config file with web socket information
+ELEMENT=$3
 
 if [  -z "$PROJECT_NAME" ]
     then
@@ -16,5 +20,16 @@ if [  -z "$DESCRIPTOR" ]
 fi
 
 echo "creating screen for project ${PROJECT_NAME}, descriptor ${DESCRIPTOR}"
+#Check if project exists before initializing it again
+#if ! screen -list | grep -q $PROJECT_NAME; then
+screen -AdmS $PROJECT_NAME -t "${DESCRIPTOR} \n"
+echo "done"
+#else
+#    echo "Screen already start, adding extra websocket......"
+#fi
+#create the screen can run start the web socket relay
+screen -S $PROJECT_NAME -t $DESCRIPTOR -X stuff "node startWebSocket.js '${ELEMENT}' \n"
 
-screen -S $PROJECT_NAME -t $DESCRIPTOR
+echo "done"
+
+
